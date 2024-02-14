2000d4a3e5d74707ec4ccc60666938d9b41d1867
sivaramprasad.madduri@gmail.com <sivaramprasad.madduri@gmail.com>
2024-02-14 19:36:54 +0530

    Completed student related changes

---
+++ src/app/students/students.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { StudentService } from './student.service';
+import { Student } from '../models/ui-models/student.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  students: Student[] = [];
+  displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+  filterString = '';
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit(): void {
+    //fetch students
+    this.studentService.getStudents().subscribe(
+      (SuccessResponse) => {
+        this.students = SuccessResponse;
+        this.dataSource = new MatTableDataSource<Student>(this.students);
+        if (this.matPaginator) {
+          this.dataSource.paginator = this.matPaginator;
+        }
+
+        if (this.matSort) {
+          this.dataSource.sort = this.matSort;
+        }
+      },
+      (errorResponse) => {
+        console.log(errorResponse);
+      }
+    );
+  }
+  filtreStudent() {
+    this.dataSource.filter = this.filterString.trim().toLocaleLowerCase();
+  }
+}
